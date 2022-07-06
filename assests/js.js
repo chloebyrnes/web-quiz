0981b460cd3a2ea1bd17a453718a50819b11f041
chloebyrnes <107075274+chloebyrnes@users.noreply.github.com>
2022-07-06 15:50:02 -0400

    new

--- assests/js.js
+++ assests/js.js
@@ -164,7 +164,7 @@ function saveHighscore() {
     window.localStorage.setItem("highscores", JSON.stringify(highscores));
 
     //redirect to next page
-    window.location.href = "highscores.html"
+    window.location.href = "highscorepage.html"
 
     }
 }
