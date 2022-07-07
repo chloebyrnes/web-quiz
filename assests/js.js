ad0a88b3a8e211b4dd8fbcabfd57c76b8f653831
chloebyrnes <107075274+chloebyrnes@users.noreply.github.com>
2022-07-06 21:53:19 -0400

    finished for grading

--- assests/js.js
+++ assests/js.js
@@ -27,6 +27,7 @@ var remainingtime = "";
 
 document.getElementById("startbtn").addEventListener("click", function(){
     startquiz();
+
 });
 
 
@@ -170,3 +171,24 @@ function saveHighscore() {
 }
 
 submitBtn.onclick = saveHighscore;
+
+// Highscore
+var highscoresContainer = document.getElementById('highscores');
+
+function getHighscore() {
+    var highscores = JSON.parse(localStorage.getItem('highscores')) || [];
+    console.log(highscores)
+
+    highscoresContainer.innerHTML = ''
+
+    highscores.forEach(highscore => {
+        var listItem = document.createElement('li');
+        var initials = highscore.initials;
+        var score = highscore.score;
+        listItem.textContent = 'Initials: ' + initials + ' Highscore: ' + score;
+        highscoresContainer.appendChild(listItem)
+    })
+}
+
+getHighscore()
+
--- assests/js2.js
+++ assests/js2.js
@@ -1,34 +1,19 @@
-
-
-var scoreText = document.querySelector('#score');
-var restartbtnEl = document.querySelector("#restartbtn")
-var submitBtn = document.querySelector("#submit")
-
 // Highscore
+var highscoresContainer = document.getElementById('highscores');
 
-function saveHighscore() {
-    // get value of input box
-    var initialsEl = document.getElementById("initials");
-    var initials = initialsEl.value.trim();
+function getHighscore() {
+    var highscores = JSON.parse(localStorage.getItem('highscores')) || [];
+    console.log(highscores)
 
-    // make sure value wasn't empty
-    if (initials !== "") {
-        // get saved scores from localstorage, or if not any, set to empty array
-        var highscores =
-        JSON.parse(window.localStorage.getItem("highscores")) || [];
+    highscoresContainer.innerHTML = ''
 
-    //format new score object for current user
-    var newScore = {
-        score: time,
-        initials: initials
-    };
-
-    // save to local storage
-    highscores.push(newScore);
-    window.localStorage.setItem("highscores", JSON.stringify(highscores));
-
-    //redirect to next page
-    window.location.href = "highscorepage.html"
-
-    }
+    highscores.forEach(highscore => {
+        var listItem = document.createElement('li');
+        var initials = highscore.initials;
+        var score = highscore.score;
+        listItem.textContent = 'Initials: ' + initials + ' Highscore: ' + score;
+        highscoresContainer.appendChild(listItem)
+    })
 }
+
+getHighscore()
